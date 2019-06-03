57b34f8d8594652a097f98cc537d3e518494525c
ahlamcpp <aalmusallam@cpp.edu>
2019-06-03 11:50:18 +0300

    created game logic-js-functions

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,25 +2,53 @@
 var cards = [ "queen", "queen", "king", "king" ];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-//cards[0] = cardOne;
-cardsInPlay.push('cardOne');
-console.log("User flipped queen");
 
-var cardTwo = cards[2];
-//cards[0] = cardOne;
-cardsInPlay.push('cardTwo');
-console.log("User flipped King");
-
-
-	if(cardsInPlay.length === 2){
+function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+}
+}
 
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again.");
-	}
 
+function flipCard (cardId) {
+ console.log("User flipped " + cards[cardId]);
+ cardsInPlay.push(cards[cardId]);
 
 };
+
+
+flipCard(0);
+flipCard(2);
+checkForMatch(); 
+
+
+ // if we call it from inside the "flipCard" function, we will diffrent result than in the "Deliverable" image!! 
+ // so I did some changes of instructions 
+ // the below is the code:
+
+/*
+ function flipCard (cardId) {
+ console.log("User flipped " + cards[cardId]);
+ cardsInPlay.push(cards[cardId]);
+ checkForMatch(); 
+
+};
+
+
+flipCard(0);
+flipCard(2);*/
+
+//and the result would be:
+
+
+/* 
+User flipped queen
+main.js:10 Sorry, try again.
+main.js:16 User flipped king
+main.js:10 Sorry, try again.
+*/
+
+
 
