117f378d139ba46ffff0ca467574e47572cb661a
ahlamcpp <aalmusallam@cpp.edu>
2019-06-03 08:19:32 +0300

    created game logic-js-main.js

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,11 +1,26 @@
-console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = [ "queen", "queen", "king", "king" ];
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+//cards[0] = cardOne;
+cardsInPlay.push('cardOne');
+console.log("User flipped queen");
+
+var cardTwo = cards[2];
+//cards[0] = cardOne;
+cardsInPlay.push('cardTwo');
+console.log("User flipped King");
 
 
-console.log("User flipped " + cardOne);
+	if(cardsInPlay.length === 2){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
 
-console.log("User flipped " + cardFour);
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+
+
+};
+
