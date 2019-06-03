3237ba9b6af2d9df373cfe41c9235291183a12e6
ahlamcpp <aalmusallam@cpp.edu>
2019-06-03 13:45:26 +0300

    created game logic-js

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,54 +1,54 @@
-
-var cards = [ "queen", "queen", "king", "king" ];
+var cards = [ 
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage:	"images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage:	"images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage:	"images/king-of-diamonds.png"
+}
+];
 var cardsInPlay = [];
-
-
 function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
   console.log("You found a match!");
-} else {
+}   else {
   console.log("Sorry, try again.");
 }
-}
+};
+
 
 
 function flipCard (cardId) {
- console.log("User flipped " + cards[cardId]);
- cardsInPlay.push(cards[cardId]);
 
-};
+ console.log("User flipped " + cards[cardId].rank);
+ cardsInPlay.push(cards[cardId].rank);
+ console.log(cards[cardId].cardImage);
+ console.log(cards[cardId].suit);
+
+	if (cardsInPlay.length === 2) {
+	checkForMatch();
+}
+
+}
 
 
 flipCard(0);
 flipCard(2);
-checkForMatch(); 
 
 
- // if we call it from inside the "flipCard" function, we will diffrent result than in the "Deliverable" image!! 
- // so I did some changes of instructions 
- // the below is the code:
-
-/*
- function flipCard (cardId) {
- console.log("User flipped " + cards[cardId]);
- cardsInPlay.push(cards[cardId]);
- checkForMatch(); 
-
-};
-
-
-flipCard(0);
-flipCard(2);*/
-
-//and the result would be:
-
-
-/* 
-User flipped queen
-main.js:10 Sorry, try again.
-main.js:16 User flipped king
-main.js:10 Sorry, try again.
-*/
 
 
 
