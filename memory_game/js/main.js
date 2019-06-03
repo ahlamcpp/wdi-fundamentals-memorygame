8a900e6462c1c6c2ca2c4293c2c0aaa52925bb75
ahlamcpp <aalmusallam@cpp.edu>
2019-06-03 15:46:19 +0300

    created game logic-final

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -21,22 +21,36 @@ var cards = [
 }
 ];
 var cardsInPlay = [];
+
+function createBoard() {
+	for (var i = 0; i < cards.length; i++) {
+   
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+
+	}
+}
+
 function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-  console.log("You found a match!");
+  alert("You found a match!");
 }   else {
-  console.log("Sorry, try again.");
+  alert("Sorry, try again.");
 }
 };
 
 
 
-function flipCard (cardId) {
-
+function flipCard () {
+var cardId = this.getAttribute('data-id');
  console.log("User flipped " + cards[cardId].rank);
  cardsInPlay.push(cards[cardId].rank);
  console.log(cards[cardId].cardImage);
  console.log(cards[cardId].suit);
+ this.setAttribute('src', cards[cardId].cardImage);
 
 	if (cardsInPlay.length === 2) {
 	checkForMatch();
@@ -44,9 +58,7 @@ function flipCard (cardId) {
 
 }
 
-
-flipCard(0);
-flipCard(2);
+createBoard();
 
 
 
